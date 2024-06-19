91b5805691412543dacdbe9b2df8892e7721c93c
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2024-06-19 16:18:23 -0400

    feat: add custom button and include of login

--- src/auth/Login.tsx
+++ src/auth/Login.tsx
@@ -3,21 +3,28 @@ import CButton from "../common/components/CButton";
 
 const Login = () => {
   return (
-    <div className="h-dvh w-screen flex justify-center items-center px-5 md:px-10">
+    <div className="h-dvh w-screen flex flex-col justify-center items-center px-5 md:px-10 gap-3">
+      <h1 className="text-7xl mb-2">
+        Re<span className=" text-pignus-500">haviour</span>
+      </h1>
       <form className="flex w-full sm:w-full md:w-3/6 lg:w-1/3 flex-col gap-4">
         <div>
-          <div className="mb-2 block">
-            <Label htmlFor="rut-input" value="Tu RUT" />
+          <div className="block">
+            <Label htmlFor="rut-input" value="Tu RUT" className="text-lg" />
           </div>
           <TextInput id="rut-input" type="text" placeholder="20.767.691-k" />
         </div>
         <div>
-          <div className="mb-2 block">
-            <Label htmlFor="password-input" value="Tu contraseña" />
+          <div className="block">
+            <Label
+              htmlFor="password-input"
+              value="Tu contraseña"
+              className="text-lg"
+            />
           </div>
           <TextInput id="password-input" type="password" placeholder="****" />
         </div>
-        <CButton type="submit">Submit</CButton>
+        <CButton type="submit" className="mt-3">Submit</CButton>
       </form>
     </div>
   );
--- src/common/components/CButton.tsx
+++ src/common/components/CButton.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Button, ButtonProps } from "flowbite-react";
 
 type CButtonProps = ButtonProps & {};
@@ -7,7 +6,7 @@ const CButton: React.FC<CButtonProps> = (props) => {
   return (
     <Button
       {...props}
-      className="border border-transparent bg-pignus-500 text-white focus:ring-4 focus:ring-pignus-300 enabled:hover:bg-pignus-700"
+      className={`border border-transparent bg-pignus-500 text-white focus:ring-4 focus:ring-pignus-300 enabled:hover:bg-pignus-700 ${props.className}`}
     />
   );
 };

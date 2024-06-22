5f25c536ceb43309ee3ab0984a7073f37182014d
minskapp <ignacia.rivas@alumnos.ucn.cl>
2024-06-22 05:43:53 -0400

    fix: removed enterprise from welcome message on admin

--- src/dashboard/components/WelcomeMessage.tsx
+++ src/dashboard/components/WelcomeMessage.tsx
@@ -1,3 +1,4 @@
+import { PIGNUS_ADMIN } from "../../api/constants/roles";
 import useUserInformation from "../../auth/hooks/useUserInformation";
 
 interface Props {
@@ -5,7 +6,7 @@ interface Props {
 }
 
 const WelcomeMessage = ({ className }: Props) => {
-  const { username } = useUserInformation();
+  const { username, role } = useUserInformation();
   const enterprise = "Proyectos Castillo, Cuevas y Molina S.P.A";
 
   return (
@@ -13,9 +14,11 @@ const WelcomeMessage = ({ className }: Props) => {
       <h1 className="text-5xl md:text-7xl mb-3 md:mb-10 text-wrap text-center px-3">
         ¡Bienvenid@ <span className="text-pignus-600">{username}!</span>
       </h1>
-      <p className="text-2xl md:text-4xl text-pignusBlue-400 font-semibold px-3 text-center text-wrap mb-5 md:mb-10">
-        {enterprise}
-      </p>
+      {role !== PIGNUS_ADMIN && 
+        <p className="text-2xl md:text-4xl text-pignusBlue-400 font-semibold px-3 text-center text-wrap mb-5 md:mb-10">
+          {enterprise}
+        </p>
+      }
       <h2 className="text-center text-wrap text-2xl md:text-4xl">
         Estadísticas de <span className="text-pignus-600">Hoy</span>
       </h2>

617befc6080b1bc55891e06313f5b046d7c7388f
David Araya Cádiz <david.araya@alumnos.ucn.cl>
2024-06-22 04:40:54 -0400

    fix: adapt paddings and sizes for mobile view

--- src/dashboard/components/WelcomeMessage.tsx
+++ src/dashboard/components/WelcomeMessage.tsx
@@ -10,11 +10,14 @@ const WelcomeMessage = ({ className }: Props) => {
 
   return (
     <div className={`flex flex-col justify-start items-center ${className}`}>
-      <h1 className="text-7xl mb-10">
+      <h1 className="text-5xl md:text-7xl mb-3 md:mb-10 text-wrap text-center px-3">
         ¡Bienvenid@ <span className="text-pignus-600">{username}!</span>
       </h1>
-      <h2 className="text-4xl text-pignusBlue-400 font-semibold">
+      <p className="text-2xl md:text-4xl text-pignusBlue-400 font-semibold px-3 text-center text-wrap mb-5 md:mb-10">
         {enterprise}
+      </p>
+      <h2 className="text-center text-wrap text-2xl md:text-4xl">
+        Estadísticas de <span className="text-pignus-600">Hoy</span>
       </h2>
     </div>
   );
